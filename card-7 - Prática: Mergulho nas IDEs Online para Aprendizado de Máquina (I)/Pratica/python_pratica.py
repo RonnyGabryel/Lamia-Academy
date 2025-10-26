bfa3e7d60755e9a78eb9e832f931d1dfc5877db0
Ronny Gabryel <162502328+RonnyGabryel@users.noreply.github.com>
2025-10-26 14:34:04 -0300

    Add files via upload

---
+++ card-7 - Prática: Mergulho nas IDEs Online para Aprendizado de Máquina (I)/Pratica/python_pratica.py
@@ -0,0 +1,25 @@
+# Operações Básicas em python
+
+# Soma
+soma = 5 + 3
+print('soma:', soma)
+
+# Subtrair
+sub = 10 - 4
+print('subritação:', sub)
+
+# Mutiplicar
+mult = 6 * 7
+print('mutiplicação', mult)
+
+# Dividir
+div = 20 / 5
+print('divisão', div)
+
+# Potência
+pot = 2 ** 3
+print('Potêcia:', pot)
+
+# Modulo
+resto = 17 % 3
+print('Resto de divisão:', resto)
